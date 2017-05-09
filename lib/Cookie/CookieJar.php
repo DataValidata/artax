98b7b8180a8bd037656aae15c035799eb4317b8b
Niklas Keller <me@kelunik.com>
2017-05-09 20:32:54 +0200

    Security: Fix cookie leakage to wrong origins and cookie accept criteria
    Previously cookies of foo.bar.example.com were leaked to foo.bar. Additionally, any site could set cookies for any other site. Artax follows newer browser implementations now. Cookies can only be set on domains higher or equal to the current domain, but not on any public suffixes.

--- lib/Cookie/CookieJar.php
+++ lib/Cookie/CookieJar.php
@@ -5,6 +5,12 @@ namespace Amp\Artax\Cookie;
 interface CookieJar {
     public function get(string $domain, string $path = '', string $name = null): array;
     public function getAll(): array;
+
+    /**
+     * Note: Implicit domains MUST NOT start with a dot, explicitly set domains MUST start with a dot.
+     *
+     * @param Cookie $cookie
+     */
     public function store(Cookie $cookie);
     public function remove(Cookie $cookie);
     public function removeAll();
